2b0911b82dbd91f125579541b59bc3f3b4c78145
Rex Hu <huzhengyu1986@gmail.com>
2021-05-30 16:21:43 -0400

    Updated comments on MultiSession.scala
    Completed the examples in MnMCount.scala

--- src/main/scala/wc/MnMCount.scala
+++ src/main/scala/wc/MnMCount.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.functions._
 
 object MnMCount {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("M&M Counter").getOrCreate()
+    val spark = SparkSession.builder().master("local[2]").appName("M&M Counter").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
 
     if (args.length < 1) {
@@ -16,7 +16,8 @@ object MnMCount {
     val dataFile = args(0)
 
     val df: DataFrame = spark.read.format("csv").option("header", true).option("inferSchema", true).load(dataFile)
-    df.show()
+    println("Showing a few rows of the data.")
+    df.show(10)
 
     val countDF = df.select("State", "Color", "Count")
       .groupBy("State", "Color")
@@ -25,6 +26,11 @@ object MnMCount {
 
     countDF.show(10)
     println(s"Total rows = ${countDF.count()}")
+
+    val tempCountDF = countDF.where(col("State") === "TX")
+
+    println("Showing the aggregated data for Texas")
+    tempCountDF.show(20, false)
 
     spark.stop()
   }
